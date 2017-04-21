a8eac7328db0ef0c7b576ab288a1b534bf67cd98
Ruonan Wang <jason.ruonan.wang@gmail.com>
2017-04-21 12:37:56 -0400

    more changes to read

--- readStream.cpp
+++ readStream.cpp
@@ -10,9 +10,8 @@ void getcb(const void *data, std::string doid, std::string var,
     std::cout << "data object ID = " << doid << "\n"; // do you need to flush?
     std::cout << "variable name = " << var << "\n";
     std::cout << "data type = " << dtype << "\n";
-
     std::size_t varsize = std::accumulate(varshape.begin(), varshape.end(), 1, std::multiplies<std::size_t>());
-
+    if(varsize > 256) varsize = 256;
     for (unsigned int i = 0; i < varsize; ++i)
         std::cout << ((float *)data)[i] << " ";
     std::cout << std::endl;
@@ -20,16 +19,27 @@ void getcb(const void *data, std::string doid, std::string var,
 
 int main(int argc, char *argv[])
 {
-    string ip="127.0.0.1", port;
+    // args
+    string local_ip = "127.0.0.1";
+    string local_port = "12306";
+    string remote_ip = "127.0.0.1";
+    string remote_port = "12307";
+    string method = "zmq";
+
     if(argc >= 2){
-        ip = argv[1];
-        if(argc >= 3){
-            port = argv[2];
-        }
+        local_ip = argv[1];
     }
 
-    adios::ADIOS adios(adios::Verbose::WARN, true);
+    if(argc >= 3){
+        method = argv[2];
+    }
 
+    if(argc >= 4){
+        local_port = argv[3];
+    }
+
+    // adios
+    adios::ADIOS adios(adios::Verbose::WARN, true);
     adios::Method &datamanSettings = adios.DeclareMethod("WAN");
     if (!datamanSettings.IsUserDefined())
     {
@@ -39,22 +49,18 @@ int main(int argc, char *argv[])
                 "method_type=stream",
                 "method=zmq",
                 "monitoring=yes",
-                "local_ip=" + ip,
-                "remote_ip=127.0.0.1",
-                "local_port=12307",
-                "remote_port=12306");
+                "local_ip=" + local_ip,
+                "remote_ip=" + remote_ip,
+                "local_port" + local_port,
+                "remote_port" + remote_port);
     }
 
     auto datamanReader = adios.Open("stream", "r", datamanSettings);
-
     datamanReader->SetCallBack(getcb);
-
     while(1)
     {
         std::this_thread::sleep_for(std::chrono::milliseconds(1000));
     }
-
     datamanReader->Close();
-
     return 0;
 }
