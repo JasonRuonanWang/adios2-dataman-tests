502fd82eb29c10c6e3bb9f10fd1c7f3266f53eda
Jason Wang <wangr1@ornl.gov>
2017-04-21 16:00:39 +0000

    abstracted args for writeStream

--- writeStream.cpp
+++ writeStream.cpp
@@ -6,21 +6,34 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    string ip, port;
+    // args
+    string local_ip = "127.0.0.1";
+    string local_port = "12306";
+    string remote_ip = "127.0.0.1";
+    string remote_port = "12307";
+    string method = "zmq";
+
     if(argc >= 2){
-        ip = argv[1];
-        if(argc >= 3){
-            port = argv[2];
-        }
+        remote_ip = argv[1];
     }
 
-    adios::ADIOS adios(adios::Verbose::WARN, true);
+    if(argc >= 3){
+        method = argv[2];
+    }
+
+    if(argc >= 4){
+        remote_port = argv[3];
+    }
+
+    // data
     std::vector<float> myFloats(1024);
     const std::size_t Nx = myFloats.size();
     for(size_t i=0; i<Nx; i++){
         myFloats[i]=i;
     }
 
+    // adios
+    adios::ADIOS adios(adios::Verbose::WARN, true);
     auto ioMyFloats = adios.DefineVariable<float>("myfloats", adios::Dims{Nx});
     adios::Method &datamanSettings = adios.DeclareMethod("WAN");
     if (!datamanSettings.IsUserDefined())
@@ -28,45 +41,19 @@ int main(int argc, char *argv[])
         datamanSettings.SetEngine("DataManWriter");
         datamanSettings.SetParameters(
                 "real_time=yes",
+                "compression_method=zfp",
                 "method_type=stream",
-                "method=zmq",
+                "method=" + method,
                 "monitoring=no",
-                "local_ip=127.0.0.1",
-                "remote_ip=" + ip,
-                "local_port=12306",
-                "remote_port=12307");
+                "local_ip=" + local_ip,
+                "remote_ip=" + remote_ip,
+                "local_port" + local_port,
+                "remote_port" + remote_port);
     }
-
-
-
-
-
     auto datamanWriter = adios.Open("stream", "w", datamanSettings);
-    if (datamanWriter == nullptr)
-        throw std::ios_base::failure("ERROR: failed to create DataMan I/O engine at Open\n");
-
     for(int i=0; i<100; i++){
-        try
-        {
-            datamanWriter->Write<float>(ioMyFloats, myFloats.data());
-            datamanWriter->Close();
-        }
-        catch (std::invalid_argument &e)
-        {
-            std::cout << "Invalid argument exception, STOPPING PROGRAM\n";
-            std::cout << e.what() << "\n";
-        }
-        catch (std::ios_base::failure &e)
-        {
-            std::cout << "System exception, STOPPING PROGRAM\n";
-            std::cout << e.what() << "\n";
-        }
-        catch (std::exception &e)
-        {
-            std::cout << "Exception, STOPPING PROGRAM\n";
-            std::cout << e.what() << "\n";
-        }
-
+        datamanWriter->Write<float>(ioMyFloats, myFloats.data());
+        datamanWriter->Close();
     }
     return 0;
 }
